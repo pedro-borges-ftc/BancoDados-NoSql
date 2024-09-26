ff9140143502c9c891b0b9e6e9c5680da1792f7c
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2024-09-25 21:29:45 -0300

    fim aula 07

---
+++ Aula07/playground-1.mongodb.js
@@ -0,0 +1,26 @@
+// The current database to use.
+use('mongodbVSCodePlaygroundDB');
+
+// Search for documents in the current collection.
+db.getCollection('vendas')
+  .find(
+    {
+      /*
+      * Filter
+      * fieldA: value or expression
+      */
+    },
+    {
+      /*
+      * Projection
+      * _id: 0, // exclude _id
+      * fieldA: 1 // include field
+      */
+    }
+  )
+  .sort({
+    /*
+    * fieldA: 1 // ascending
+    * fieldB: -1 // descending
+    */
+  });
