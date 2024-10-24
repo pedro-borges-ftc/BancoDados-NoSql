8b5423033b51c8dc56576ba179e57d9c2150f319
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2024-10-23 21:45:48 -0300

    oat CRUD 5 pontos

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,20 +1,30 @@
 // The current database to use.
 use('mongodbVSCodePlaygroundDB');
 
-db.getCollection('inventory').deleteMany({ "_id": ObjectId("671058a19b097e529918080d") });
+  /*
 
+//CREATE
+db.getCollection('livros').insertMany([
+  { 'titulo': 'Primeiro Campeão Mundial','autor': 'Peuborg', 'ano': 2024,'categoria': 'Futebol', 'quantidade': 2, },
+  { 'titulo': 'TriCampeao Paulista','autor': 'Peuborg', 'ano': 2024,'categoria': 'Futebol', 'quantidade': 20, },
+  { 'titulo': 'Rumo ao Tri Brasileiro','autor': 'Peuborg', 'ano': 2024,'categoria': 'Futebol', 'quantidade': 2, },
+  { 'titulo': 'Tri da Libertadores','autor': 'Peuborg', 'ano': 2024,'categoria': 'Futebol', 'quantidade': 2, },
+]);
 
-// Search for documents in the current collection.
-db.getCollection('inventory')
-  .find(
+ // READ
+ db.getCollection('livros')
+ .find()
+ .sort();
+
+// UPDATE
+  db.getCollection('livros').updateMany(
+    { quantidade: { $lt: 20 } },
     {
-
-    },
-    {
-
+      $set: { 'ano': 2020},
+      $currentDate: { lastModified: true }
     }
-  )
-  .sort({
+  );
 
-  });
-  
+  //DELETE
+  db.getCollection('livros').deleteOne({ "_id": ObjectId("6719943f41ee64c6d362bc73") });
+*/
