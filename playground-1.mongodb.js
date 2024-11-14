38817dcd9503f749c15fb7c6f6f61f7c2d176c27
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2024-11-13 22:50:56 -0300

    fim aula 11

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,30 +1,16 @@
 // The current database to use.
 use('mongodbVSCodePlaygroundDB');
 
-const livrosPeuborg = db.livros.find({"autor": {$eq: "Peuborg"}}).toArray();
+db.livros.aggregate([
 
-console.log(typeof(livrosPeuborg))
+  { $group: { _id: "$categoria", totalLivros: { $sum: 1 } } },
 
-livrosPeuborg.forEach((livro, indice) => {
-  console.log(`livro ${indice + 1}: ${livro.titulo}`);
-});
+  { $sort: { totalLivros: 1 } },
 
-console.log(livrosPeuborg);
+  { $limit: 3 }
 
-  /*
-db.getCollection('livros').insertMany([
-  { "titulo": "Aula online", "autor": "FULANO DE TALZ", "ano": 2020, "categoria": "Futebol", "quantidade": 5 },
 ]);
 
-db.getCollection('livros')
-.find({"autor": {$eq: "FULANO DE TALZ"}})
-.sort();
+/*
 
-const livrosPeuborg = db.livros.find({"autor": {$eq: "Peuborg"}}).count();
-
-console.log(`${livrosPeuborg} livros foram encontrados.`);
-
-console.log(typeof(livrosPeuborg))
-
-console.log(livrosPeuborg);
 */
