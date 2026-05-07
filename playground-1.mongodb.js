aa93a82aca63916df5a0083a080f55a1a97a2690
peuborg <peuborg.8@gmail.com>
2026-05-06 21:33:58 -0300

    fim aula 09 exercicio 02

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,13 +1,10 @@
 // The current database to use. --mongodbVSCodePlaygroundDB
 use('mongodbVSCodePlaygroundDB');
 
-// Exercício 2
-// Encontrar livros de não-ficção com quantidade maior que 5 e publicados após 2015.
-db.livros.find({ 
-  categoria: { $ne: "Ficção Científica"}, 
-  quantidade: { $gt: 5 }, 
-  ano: { $gt: 2015 } 
-})
+
+db.livros.find({
+  "categoria": { $nin: ["Fantasia", "Ficção Científica"] }
+});
 
 
 /*
