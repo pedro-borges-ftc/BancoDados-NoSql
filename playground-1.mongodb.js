94307115f4bb9ffa052521930b0a0beacff83888
peuborg <peuborg.8@gmail.com>
2026-05-27 21:22:37 -0300

    fim lista exercicio 05 2026.1

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,8 +1,14 @@
 // The current database to use. --mongodbVSCodePlaygroundDB
 use('mongodbVSCodePlaygroundDB');
 
-// Lista 03 - Q2
-db.livros.find({
-  "categoria": { $in: ["Tecnologia", "Ciência"] },
-  "quantidade": { $gt: 6 }
-});
+db.livros.aggregate([
+  // Agrupar os livros por categoria e calcular a média de exemplares
+  { $group: { _id: "$categoria", mediaExemplares: { $avg: "$quantidade" } } },
+  // Ordenar as categorias pela maior média de exemplares
+  { $sort: { mediaExemplares: -1 } },
+  // Limitar a consulta às 3 categorias com maior média
+  { $limit: 5 }
+]);
+
+
+
